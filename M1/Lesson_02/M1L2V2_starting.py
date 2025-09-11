2c38d5f650a24661de3dd91b5163474f7c535e6c
Lampros Velentzas <labrosvel@yahoo.gr>
2025-09-11 17:05:17 +0100

    Adding further interactivity to the code

--- M1/Lesson_02/M1L2V2_starting.py
+++ M1/Lesson_02/M1L2V2_starting.py
@@ -3,6 +3,18 @@ from dotenv import load_dotenv
 from openai import OpenAI
 import streamlit as st
 
+
+@st.cache_data
+def get_response(user_prompt, temperature):
+    response = client.responses.create(
+        model="gpt-4.1-mini",
+        input=[{"role": "user", "content": user_prompt}],  # Prompt
+        temperature=temperature,  # A bit of creativity
+        max_output_tokens=100,  # Limit response length
+    )
+    return response
+
+
 # load environment variables from .env file
 load_dotenv()
 
@@ -12,11 +24,23 @@ client = OpenAI()
 st.title("Hello, GenAI!")
 st.write("This is your first Streamlit app.")
 
-response = client.responses.create(
-    model="gpt-4.1-mini",
-    input=[{"role": "user", "content": "Explain generative AI in one sentence."}],
-    temperature=0.7,  # A bit of creativity
-    max_output_tokens=100,  # Limit response length
+# Add a text input box for the user prompt
+user_prompt = st.text_input(
+    "Enter your prompt:", "Explain generative AI in one sentence."
 )
 
-st.write(response.output_text)
+# Add a slier for temperature
+temperature = st.slider(
+    "Model temperature:",
+    min_value=0.0,
+    max_value=1.0,
+    value=0.7,
+    step=0.01,
+    help="Controls reandomness: 0 = deterministic, 1 = very creative",
+)
+
+with st.spinner("AI is working..."):
+    response = get_response(user_prompt, temperature)
+
+    # print the response from OpenAI
+    st.write(response.output_text)
